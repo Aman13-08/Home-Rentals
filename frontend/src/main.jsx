bb10de3985270b40a5a5f5cb08e44cf5196c5f1c
Aman tiwari <amantiwari7462@gmail.com>
2024-08-29 23:36:24 +0530

    Login fuctionality and redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
+import React from 'react'
+import ReactDom from "react-dom/client"
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {Provider} from "react-redux"
+import { persistor ,store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
+    <PersistGate store={store}  persistor={persistor}>
     <App />
-  </StrictMode>,
+    </PersistGate>
+  </Provider>,
 )
