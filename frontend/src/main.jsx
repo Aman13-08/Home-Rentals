6326ebc2425da8157637f59da694dd5e7d4567a3
Aman tiwari <amantiwari7462@gmail.com>
2024-09-01 22:38:46 +0530

    Navbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import ReactDom from "react-dom/client"
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client"
 import App from './App.jsx'
-import './index.css'
+import './index.css';
 import {Provider} from "react-redux"
 import { persistor ,store } from './redux/store.js'
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from "redux-persist/integration/react"
 
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate store={store}  persistor={persistor}>
     <App />
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+
 
 const HomePages = () => {
   return (
     <div>
-      HomePage
+      <Navbar/>
     </div>
   )
 }
